44cf4f2c9641ba4fc7d2746a1050ab32f66e7fea
m4rcTr3y <marctrevis61@proton.me>
2025-11-25 12:51:49 +0300

    feat: guard manage.php when composer deps missing

--- packages/skeleton/manage.php
+++ packages/skeleton/manage.php
@@ -6,9 +6,20 @@ if (!defined('REUT_PROJECT_ROOT')) {
 
 chdir(REUT_PROJECT_ROOT);
 
-require REUT_PROJECT_ROOT . '/vendor/autoload.php';
+$autoload = REUT_PROJECT_ROOT . '/vendor/autoload.php';
+if (!file_exists($autoload)) {
+    fwrite(STDERR, "vendor/autoload.php not found. Run `composer install` inside this project before using the CLI.\n");
+    exit(1);
+}
+
+require $autoload;
 
 use Reut\DB\Creator\DatabaseCreator;
+
+if (!class_exists(DatabaseCreator::class)) {
+    fwrite(STDERR, "Composer dependencies are missing. Run `composer install` to install reut/core and other packages.\n");
+    exit(1);
+}
 
 DatabaseCreator::Generate();
 
